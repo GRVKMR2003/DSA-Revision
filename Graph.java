51848c0b340acbfe61c9d061d743fef11956c83a
Gaurav Kumar <gk9596355299@gmail.com>
2026-02-21 17:16:15 +0530

    AdjancyList Creation

--- Graph.java
+++ Graph.java
@@ -6,35 +6,32 @@ public class Graph {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter number of vertices: ");
-        int vertices = sc.nextInt();
+        int vert = sc.nextInt();
 
         System.out.print("Enter number of edges: ");
-        int edges = sc.nextInt();
+        int e = sc.nextInt();
 
-        List<List<Integer>> adjList = new ArrayList<>();
-
-        for (int i = 0; i < vertices; i++) {
-            adjList.add(new ArrayList<>());
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<vert;i++){
+            adj.add(new ArrayList<>());
         }
 
-        System.out.println("Enter edges (src dest):");
-        for (int i = 0; i < edges; i++) {
-            int src = sc.nextInt();
-            int dest = sc.nextInt();
+         for(int i=0;i<e;i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adj.get(u).add(v);
 
-            adjList.get(src).add(dest);
-            adjList.get(dest).add(src);
-        }
+         }
 
-        System.out.println("\nAdjacency List:");
-        for (int i = 0; i < vertices; i++) {
+           for (int i = 0; i < vert; i++) {
             System.out.print(i + " -> ");
-            for (int neighbor : adjList.get(i)) {
+            for (int neighbor : adj.get(i)) {
                 System.out.print(neighbor + " ");
             }
             System.out.println();
         }
 
         sc.close();
+
     }
 }
